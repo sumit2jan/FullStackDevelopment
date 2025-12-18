087712be1ca309392422a2482b5ff537743340d8
sumit2jan <ss9779915666@gmail.com>
2025-12-18 22:54:06 +0530

    feat: complete project with all planned features

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,9 +1,19 @@
 const button = document.querySelectorAll(".drum");
 for(let i = 0; i<button.length; i++){ 
 button[i].addEventListener("click",function(){
-    
     let btnInnerhtml = this.innerHTML;
-    switch (btnInnerhtml) {
+    makeSound(btnInnerhtml);
+    buttonAnimation(btnInnerhtml);
+});
+}
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+   switch (key) {
         case "w":
             let Sound1 = new Audio("sounds/tom-1.mp3");
             Sound1.play();
@@ -35,5 +45,53 @@ button[i].addEventListener("click",function(){
         default: console.log(btnInnerhtml);
             
     }
-});
 }
+
+function buttonAnimation(value){
+    let activebutton = document.querySelector("."+value);
+    activebutton.classList.add("pressed");
+
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    },100);
+}
+
+// document.addEventListener('keydown', function(event) {
+    
+//     if (event.key === 'w') {
+//         event.preventDefault(); 
+//         let Sound1 = new Audio("sounds/tom-1.mp3");
+//         Sound1.play();
+//     }
+//     if (event.key === 'a') {
+//         event.preventDefault(); 
+//         let Sound2 = new Audio("sounds/tom-2.mp3");
+//         Sound2.play();
+//     }
+//     if (event.key === 's') {
+//         event.preventDefault(); 
+//         let Sound3 = new Audio("sounds/tom-3.mp3");
+//         Sound3.play();
+//     }
+//     if (event.key === 'd') {
+//         event.preventDefault(); 
+//         let Sound4 = new Audio("sounds/tom-4.mp3");
+//         Sound4.play();
+//     }
+//     if (event.key === 'j') {
+//         event.preventDefault(); 
+//         let Sound5 = new Audio("sounds/snare.mp3");
+//         Sound5.play();
+//     }
+//     if (event.key === 'k') {
+//         event.preventDefault(); 
+//         let Sound6 = new Audio("sounds/crash.mp3");
+//         Sound6.play();
+
+//     }
+//     if (event.key === 'l') {
+//         event.preventDefault(); 
+//         let Sound7 = new Audio("sounds/kick-bass.mp3");
+//         Sound7.play();
+//     }
+// });
