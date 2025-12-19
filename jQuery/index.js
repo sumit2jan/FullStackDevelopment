173a106314e8287357cae6750415370b168949c8
sumit2jan <ss9779915666@gmail.com>
2025-12-19 23:31:36 +0530

    feat: add jQuery practice files

---
+++ jQuery/index.js
@@ -0,0 +1,5 @@
+//$("h1").css("color","red"); not good.
+$("h1").addClass("new-class")
+//hasClass,removeClasss,("new-class new-classtwo")this is how we add two classes.
+$("h1").text("HeHeHeHe")//to change the text.
+$("h1").html("<em>hey</em>")//to use the html elements and change text.
