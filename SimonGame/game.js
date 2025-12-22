9e7ffbffb8777d5c09e829544e12439f162b54c4
sumit2jan <ss9779915666@gmail.com>
2025-12-22 23:46:03 +0530

    feat: added new features

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -1,5 +1,16 @@
+let userClickedPattern = [];
 let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
+
+
+ //Button Trigger Handling Function
+    $(".btn").on("click",function(){
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    }); 
+    
 function nextSequence(){
 
     let randomNumber = Math.floor(Math.random()*4);
@@ -15,14 +26,22 @@ function nextSequence(){
     playSound(randomChosenColour);
 
 }
+
  //creating function to play the sounds
  function playSound(val){
    let audio = new Audio("sounds/" + val + ".mp3");
    audio.play();
  }
 
+ //creating the funtion for Button Animation.
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
  
- $(document).keydown(function(){
-    nextSequence();
-}); 
+//  $(document).keydown(function(){
+//     nextSequence();
+// }); 
 //  nextSequence();
