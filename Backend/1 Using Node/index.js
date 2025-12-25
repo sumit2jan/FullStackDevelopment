c77aa3d06b6061f5da16de51a10bb1a9cb0944de
sumit2jan <ss9779915666@gmail.com>
2025-12-25 23:06:04 +0530

    feat: add node.js practice files

---
+++ Backend/1 Using Node/index.js
@@ -0,0 +1,1 @@
+console.log("Hello now i am going to learn the node.js");
---
+++ Backend/2 Native Modules/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+// fs.writeFile('text.txt',"Hello Dunia",(err)=>{
+//     if(err) throw err;
+//     console.log("The file has been saved!");
+// })
+
+fs.readFile('text.txt',"utf8",(err,data)=>{
+    if(err) throw err;
+    console.log(data);
+})
